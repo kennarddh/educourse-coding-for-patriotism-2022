c66658f30d97a9e36715cb6b95a2b7f6c13fbac9
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-11-19 12:49:50 +0700

    feat: Set article id on card click

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,16 +1,24 @@
-import { FC } from 'react'
+import { FC, useContext } from 'react'
+
+import { PagesContext } from 'Contexts/Pages/Pages'
 
 import type { Props } from './Types'
 
 import { Container, Image, Content, Name, Button } from './Styles'
 
 const Card: FC<Props> = ({ imagePath, imageAlt, id, name }) => {
+	const { SetSelectedArticleId } = useContext(PagesContext)
+
+	const OnClick = () => {
+		SetSelectedArticleId(id)
+	}
+
 	return (
 		<Container>
 			<Image src={imagePath} alt={imageAlt} />
 			<Content>
 				<Name>{name}</Name>
-				<Button>Baca selengkapnya</Button>
+				<Button onClick={OnClick}>Baca selengkapnya</Button>
 			</Content>
 		</Container>
 	)
