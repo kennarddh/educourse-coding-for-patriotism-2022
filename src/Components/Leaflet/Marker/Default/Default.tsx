594208b3032c6bdbfc5fb577de1be4b0e00a5144
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-11-19 23:06:55 +0700

    feat: Change default marker icon anchor

--- src/Components/Leaflet/Marker/Default/Default.tsx
+++ src/Components/Leaflet/Marker/Default/Default.tsx
@@ -6,6 +6,9 @@ import iconShadow from 'leaflet/dist/images/marker-shadow.png'
 const Default = Leaflet.icon({
 	iconUrl: icon,
 	shadowUrl: iconShadow,
+	iconSize: [25, 40],
+	shadowSize: [40, 40],
+	iconAnchor: [12.5, 40],
 })
 
 export default Default
