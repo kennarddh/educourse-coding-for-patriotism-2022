547182ec49371ec8101de38f4bb4e96d9f24e022
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-11-22 13:40:32 +0700

    fix: Wrong answer

--- src/Constants/Quiz.ts
+++ src/Constants/Quiz.ts
@@ -16,8 +16,8 @@ const Quiz: IQuiz = {
 		question: 'Kapan Indonesia merdeka?',
 		time: 5,
 		answers: new Set([
-			'17 Agustus 1954',
 			'17 Agustus 1945',
+			'17 Agustus 1954',
 			'16 Agustus 1945',
 			'16 Agustus 1954',
 		]),
