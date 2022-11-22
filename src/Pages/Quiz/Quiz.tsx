e71bebc88020b0560975ab75839979bdf27caff3
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-11-22 13:30:05 +0700

    feat: Load question

--- src/Pages/Quiz/Quiz.tsx
+++ src/Pages/Quiz/Quiz.tsx
@@ -1,11 +1,15 @@
 /* eslint-disable security/detect-object-injection */
-import { FC, useContext, useEffect } from 'react'
+import { FC, useCallback, useContext, useEffect, useState } from 'react'
+
+import QuizQuestion from 'Constants/Quiz'
 
 import PagesContext from 'Contexts/Pages/Pages'
 
 import Footer from 'Components/Footer/Footer'
 import BackToTop from 'Components/BackToTop/BackToTop'
 import useTitle from 'Hooks/useTitle'
+
+import { PickUnique, Shuffle } from 'Utils/Array'
 
 import {
 	Header,
@@ -24,15 +28,34 @@ import {
 const Quiz: FC = () => {
 	const { SetIsQuiz } = useContext(PagesContext)
 
+	const [Score, SetScore] = useState<number>(0)
+
+	const [QuestionId, SetQuestionId] = useState<string>('')
+	const [AnswerChoices, SetAnswerChoices] = useState<string[]>([''])
+
 	useTitle('Kuis')
 
 	const OnBack = () => {
 		SetIsQuiz(false)
 	}
 
+	const ChangeQuestion = useCallback(() => {
+		SetQuestionId(prev => {
+			const newId = PickUnique(Object.keys(QuizQuestion), prev)
+
+			SetAnswerChoices(Shuffle([...QuizQuestion[newId].answers]))
+
+			return newId
+		})
+	}, [])
+
 	useEffect(() => {
 		window.scrollTo(0, 0)
 	}, [])
+
+	useEffect(() => {
+		ChangeQuestion()
+	}, [ChangeQuestion])
 
 	return (
 		<>
@@ -43,25 +66,20 @@ const Quiz: FC = () => {
 				<Title>Kuis</Title>
 				<QuizContainer>
 					<QuizContainerHeader>
-						<Question>Kapan Dr. Ir. H. Soekarno Lahir?</Question>
+						<Question>
+							{QuizQuestion[QuestionId]?.question ?? ''}
+						</Question>
 						<QuizContainerHeaderLeft>
-							<Text>Skor: 0</Text>
+							<Text>Skor: {Score}</Text>
 							<Text>Waktu : 10</Text>
 						</QuizContainerHeaderLeft>
 					</QuizContainerHeader>
 					<Answers>
-						<Answer isAnswered>
-							<p>1</p>
-						</Answer>
-						<Answer isAnswered>
-							<p>2</p>
-						</Answer>
-						<Answer isAnswered>
-							<p>3</p>
-						</Answer>
-						<Answer isCorrect isAnswered>
-							<p>4</p>
-						</Answer>
+						{AnswerChoices.map(choice => (
+							<Answer key={choice}>
+								<p>{choice}</p>
+							</Answer>
+						))}
 					</Answers>
 				</QuizContainer>
 			</Content>
