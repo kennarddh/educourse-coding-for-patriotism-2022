cd6088ed60c25969ec5131b42a842667b97eb695
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-11-22 19:48:37 +0700

    feat: Remove lazy load because cannot use import.meta outside module
    Need web server

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,19 @@
-import { FC, useContext, lazy, Suspense } from 'react'
+import { FC, useContext } from 'react'
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const CustomLazy = (result: any, delay = 0) =>
-	lazy(() => {
-		return new Promise(resolve => {
-			setTimeout(() => resolve(result), delay)
-		})
-	})
-
-const Article = CustomLazy(import('Pages/Article/Article'), 100)
-const Home = CustomLazy(import('Pages/Home/Home'), 100)
-const Quiz = CustomLazy(import('Pages/Quiz/Quiz'), 100)
+import Article from 'Pages/Article/Article'
+import Home from 'Pages/Home/Home'
+import Quiz from 'Pages/Quiz/Quiz'
 
 import PagesContext from 'Contexts/Pages/Pages'
-
-import Loading from 'Components/Loading/Loading'
 
 const App: FC = () => {
 	const { SelectedArticleId, IsQuiz } = useContext(PagesContext)
 
-	return (
-		<Suspense fallback={<Loading />}>
-			{IsQuiz ? <Quiz /> : SelectedArticleId ? <Article /> : <Home />}
-		</Suspense>
-	)
+	if (IsQuiz) return <Quiz />
+
+	if (SelectedArticleId) return <Article />
+
+	return <Home />
 }
 
 export default App
--- src/Components/Loading/Types.ts
+++
@@ -1,3 +0,0 @@
-export interface ILoaderRing {
-	size: number
-}
